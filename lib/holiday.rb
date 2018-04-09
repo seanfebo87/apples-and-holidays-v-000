d3b42873c3a73645818482f6637729cf1557edb9
Sean Febo <seanfebo@techie.com>
2018-04-09 21:46:01 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -65,7 +65,7 @@ holiday_hash.each do |season, info|
   info.each do |holiday, supply|
     hday = holiday.to_s.split("_").each do |stuff|
     stuff.capitalize!
-    hday.join[" "]
+    hday.join(" ")
     puts "#{hday}: #{supply}"
 end
 end
