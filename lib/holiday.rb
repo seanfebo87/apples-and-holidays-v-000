acab558b100a8dd8d85f31b2e954a8d52f2ecfab
Sean Febo <seanfebo@techie.com>
2018-04-09 20:10:21 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -60,7 +60,8 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-holiday_hash.each do |season, values|
+holiday_hash.each do |season, holiday|
+  puts season.to_s + ":"
   values.each do |holiday, supplies|
 end
 end
